e83fb8f579f07ba2e6b04effb31ac074b023a154
PeerChristensen <hr.pchristensen@gmail.com>
2022-07-06 11:06:37 +0200

    added demo file for handling UI, improvements to css styling

--- app/app.R
+++ app/app.R
@@ -13,8 +13,16 @@ library(fresh)
 ui <- dashboardPage(
   title = "SEGES App",
   dashboardHeader(
-    title = fluidRow(column(3),column(9,
-                                      span(img(height = 40, width = 180, src = "logo-seges.png"))))
+    tags$li(class = "dropdown",
+            tags$style(".main-header {max-height: 75px}"),
+            tags$style(".main-header .logo {height: 75px;padding-top:10px;padding-right:100px;}"),
+            tags$style(".main-sidebar {padding-top: 75px;}"),
+            tags$style(".sidebar-toggle {height: 75px; padding-top: 1px !important;}"),
+            tags$style(".navbar {min-height:75px !important;line-height:3; font-size:20px;}")
+    ),
+    title = fluidRow(
+      column(3),
+      column(9,span(img(height = 55, width = 220, src = "logo-seges.png"))))
   ),
   dashboardSidebar(collapsed = F,
                    uiOutput("menu")
@@ -25,6 +33,7 @@ ui <- dashboardPage(
       tags$style("h2 {color:#555555;}; h4 {color:#555555;}; p {color:#555555;}"),
       tags$style(type = "text/css", "a{color: #00511D;}")
     ),
+    
     useShinyjs(),
     tabItems(
       uiOutput("tabs")
